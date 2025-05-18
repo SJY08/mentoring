7b2886afd0655dff3ea2eac603f192ee6428c3f8
서지유 <jihoseo2008@gmail.com>
2025-05-18 23:28:48 +0900

    setting

---
+++ web/next.config.ts
@@ -0,0 +1,10 @@
+const nextConfig = {
+    reactStrictMode: false,
+    images: {},
+    rewrites: async () => [
+        {
+            source: "/api/:path*",
+            destination: process.env.BASE_URL + "/api/:path*",
+        },
+    ],
+}
