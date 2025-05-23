383b010ef62ab00a54f40658cd2ac1c9360ec92e
서지유 <jihoseo2008@gmail.com>
2025-05-23 19:58:36 +0900

    feat :: data
    dummy data

---
+++ web/src/dummy/data.ts
@@ -0,0 +1,16 @@
+export const dummy = [
+    {
+        id: 1,
+        title: "첫 번째 글",
+        username: "홍길동",
+        date: new Date().toISOString(),
+        content: "내용 1",
+    },
+    {
+        id: 2,
+        title: "두 번째 글",
+        username: "김철수",
+        date: new Date().toISOString(),
+        content: "내용 2",
+    },
+]
